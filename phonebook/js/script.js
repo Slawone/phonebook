8c3984844fe4edb041257aca07ca9cb0e57beb7c
Slawone <for1s8@ya.ru>
2023-04-03 22:08:00 +0300

    create header, logo, btns, footer

--- phonebook/js/script.js
+++ phonebook/js/script.js
@@ -1,2 +1,103 @@
 'use strict';
 
+{
+  const btnParams = [
+    {
+      className: 'btn btn-primary mr-3',
+      type: 'button',
+      text: 'Добавить',
+    },
+    {
+      className: 'btn btn-danger',
+      type: 'button',
+      text: 'Удалить',
+    },
+  ];
+
+  const createContainer = () => {
+    const container = document.createElement('div');
+    container.classList.add('container');
+    return container;
+  };
+
+  const createHeader = () => {
+    const header = document.createElement('header');
+    header.classList.add('header');
+
+    const headerContainer = createContainer();
+    header.append(headerContainer);
+
+    header.headerContainer = headerContainer;
+
+    return header;
+  };
+
+  const createLogo = title => {
+    const h1 = document.createElement('h1');
+    h1.classList.add('logo');
+    h1.textContent = `Телефонный справочник. ${title}`;
+
+    return h1;
+  };
+
+  const createMain = () => {
+    const main = document.createElement('main');
+    const mainContainer = createContainer();
+
+    main.append(mainContainer);
+    main.mainContainer = mainContainer;
+
+    return main;
+  };
+
+  const createButtonsGroup = params => {
+    const btnWrapper = document.createElement('div');
+    btnWrapper.classList.add('btn-wrapper');
+
+    const btns = params.map(({className, type, text}) => {
+      const button = document.createElement('button');
+      button.className = className;
+      button.type = type;
+      button.textContent = text;
+
+      return button;
+    });
+
+    btnWrapper.append(...btns);
+
+    return {
+      btnWrapper,
+      btns,
+    };
+  };
+
+  const createTable = () => {
+    const table = document.createElement('table');
+
+    return table;
+  };
+
+  const createFooter = title => {
+    const footer = document.createElement('footer');
+    footer.classList.add('footer');
+    footer.textContent = `Все права защищены ©${title}`;
+
+    return footer;
+  };
+
+  const init = (selectorApp, title) => {
+    const app = document.querySelector(selectorApp);
+    const header = createHeader();
+    const logo = createLogo(title);
+    const main = createMain();
+    const buttonGroup = createButtonsGroup(btnParams);
+    const table = createTable();
+    const footer = createFooter(title);
+
+    header.headerContainer.append(logo);
+    main.mainContainer.append(buttonGroup.btnWrapper, table);
+    app.append(header, main, footer);
+  };
+
+  window.phoneBookInit = init;
+}
